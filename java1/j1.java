11c7b450dd5e049c05dfa1fbaaadf7a42a0fdc8a
prem1604 <164459765+prem1604@users.noreply.github.com>
2024-12-16 15:06:21 +0530

    Update j1.java

--- java1/j1.java
+++ java1/j1.java
@@ -1,3 +1,15 @@
 public class j1 {
-    
+    public static void main(String[] args) {
+        String Name="Prem";
+        int ID=15;
+        int Age=23;
+        float studentFee=75.25f;
+        char Grade='B';
+
+        System.out.println("student name:" +Name);
+        System.out.println("student id:" +ID);
+        System.out.println("student age:" +Age);
+        System.out.println("studentfee:" +studentFee);
+        System.out.println("student grade" +Grade);
+    }
 }
